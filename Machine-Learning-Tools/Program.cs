5c30360d30ae7023755a0f19e0709c9af387f3b7
georgii sirotenko <sirotenkoegor2001@yandex.com>
2022-09-02 21:51:12 +0200

    Update Program.cs for command line

--- Machine-Learning-Tools/Program.cs
+++ Machine-Learning-Tools/Program.cs
@@ -1,11 +1,32 @@
-﻿using Mathematics;
+﻿using MatthiWare.CommandLine;
+using Mathematics;
+
 public class Program
 {
 
     
     
-    static void Main()
+    static void Main(string[] args)
     {
-        
+        // Command line parser options:
+        // "--" before parameter name,
+        // "=" after parameter name.
+        var options = new CommandLineParserOptions
+        {
+            PrefixLongOption = "--",
+            PostfixOption = "=",
+        };
+
+        var parser = new CommandLineParser<Options.ModelOptions>(options);
+        var argumentParsing = parser.Parse(args);
+
+        if (argumentParsing.HasErrors)
+        {
+            Console.Error.WriteLine("Wrong parsing!");
+            return;
+        }
+
+        // Parsed args.
+        var arguments = argumentParsing.Result;
     }
 }
