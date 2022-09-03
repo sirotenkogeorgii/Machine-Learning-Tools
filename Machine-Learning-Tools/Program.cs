c94872bc4d3e225b7e2b833c1251fcc3b4d28f90
georgii sirotenko <sirotenkoegor2001@yandex.com>
2022-09-03 17:28:29 +0200

    Change epoch number by one in console

--- Machine-Learning-Tools/Program.cs
+++ Machine-Learning-Tools/Program.cs
@@ -1,11 +1,11 @@
-﻿using MatthiWare.CommandLine;
+﻿using Datasets;
+using MatthiWare.CommandLine;
 using Mathematics;
+using Models;
+using MatthiWare.CommandLine.Core.Models;
 
 public class Program
 {
-
-    
-    
     static void Main(string[] args)
     {
         // Command line parser options:
@@ -28,5 +28,16 @@ public class Program
 
         // Parsed args.
         var arguments = argumentParsing.Result;
+
+        var dataset = new Mnist();
+        var model = new MnistModel(arguments);
+
+
+        for (int epoch = 0; epoch < arguments.Epochs; epoch++)
+        {
+            model.TrainEpoch(dataset.Train);
+            float[] accCorrects = model.Evaluate(dataset.Test);
+            Console.WriteLine($"Epoch: {epoch + 1}, Accuracy: {accCorrects[0]}, Corrects: {accCorrects[1]}");
+        }
     }
 }
